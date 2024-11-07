008f20eab1466507f7245b00c83d0037e02c3bbf
Demortes <kdethlefs@gmail.com>
2024-11-06 20:51:46 -0600

    Uploading current changes to trigger test action

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,1 +1,11 @@
-document.querySelector("img");
+const images = document.querySelector("img");
+
+if(images){
+    images.forEach(image => {
+        // Check image for fbcdn or other domains, then check 
+        // if AI/Deepfake using something like sightengine.
+        // Requires api secret, might need to involve an API
+        // Keep costs under control, a login? 
+
+    });
+}
--- functions/detect.py
+++ functions/detect.py
@@ -1,17 +1,21 @@
 import azure.functions as func
 import logging
+import json
+import requests
+import os
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
+@app.function_name(name="Detect")
 @app.route(route="http_trigger")
-def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
+def detect(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
+    imgUrl = req.params.get('url')
     params = {
         'url': 'https://sightengine.com/assets/img/examples/example-prop-c1.jpg',
         'models': 'genai',
-        'api_user': '{api_user}',
-        'api_secret': '{api_secret}'
+        'api_user': os.getenv("sightengine_api_user"),
+        'api_secret': os.getenv("sightenvigne_api_secret")
     }
     r = requests.get('https://api.sightengine.com/1.0/check.json', params=params)
 
