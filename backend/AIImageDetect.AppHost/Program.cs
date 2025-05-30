e03e772ae2238059a14a375d43aec233cb2b0615
Kevin Dethlefs <kdethlefs@gmail.com>
2025-05-30 02:15:53 -0500

    fix: Update package references to latest versions in project files

--- backend/AIImageDetect.AppHost/Program.cs
+++ backend/AIImageDetect.AppHost/Program.cs
@@ -1,6 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgres = builder.AddPostgres("postgres-db").AddDatabase("ai-image_detect");
+var postgres = builder.AddPostgres("postgres-db").AddDatabase("aiImageDetect");
 
 builder.AddProject<Projects.AIImageDetect_Api>("aiimagedetect-api")
     .WithReference(postgres);
